30bd9960ad57026d06d6d969868f297dc0be859b
kitagawaowo <uwu20201b980@gmail.com>
2023-05-16 15:54:45 -0500

    Card with elements

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: const Center(
+      body: Center(
         child:
         TodoList() 
       ),
--- lib/pages/todo_list.dart
+++ lib/pages/todo_list.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 class TodoList extends StatelessWidget {
-  const TodoList({super.key});
+  TodoList({super.key});
 
+  final names = ["Ana", "Juan", "Carlos"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("To do App"),
       ),
-      body: ListView(
+      /*body: ListView(
         children: const [
           ListTile(title: Text("List 1"), subtitle: Text("Detail list 1"),),
           ListTile(leading: Icon(Icons.anchor)),
@@ -18,6 +19,15 @@ class TodoList extends StatelessWidget {
           ListTile(leading: CircleAvatar(backgroundImage: NetworkImage("https://avatars.githubusercontent.com/u/128727709?s=400&u=8fdd3a2adfe01e35d376966deafd4b769d155ee5&v=4"),))
         ],
       ),
+      */
+      body: ListView.builder(
+        itemCount: names.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(title: Text(names[index])),
+          );
+        },
+      )
     );
   }
 }
